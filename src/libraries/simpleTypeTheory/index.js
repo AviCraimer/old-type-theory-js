28633ad2f6a51dba841dbf43701a106967804603
unknown <richardsouthwell254@gmail.com>
2020-12-01 11:49:46 +0000

    spelling corrections and started simpler type theory code

--- src/libraries/simpleTypeTheory/index.js
+++ src/libraries/simpleTypeTheory/index.js
@@ -2,7 +2,8 @@ import avi from "./avi"
 import richard from "./richard"
 import richob from "./richob"
 import richdone from "./richdone"
+import richsimp from "./richsimp"
 
 export const simpleTypeTheory = {
-    richard, avi, richob, richdone
+    richard, avi, richob, richdone, richsimp
 };
---
+++ src/libraries/simpleTypeTheory/richsimp.js
@@ -0,0 +1,19 @@
+import _ from "lodash"
+import richard from "./richard";
+
+const richsimp = {}
+
+// implementation of new, simpler, type theory
+
+richsimp.emptyContext = {
+    kind: "context",
+    entries: []
+}
+
+
+
+//warnings and tests need adding above
+
+export default richsimp;
+
+
