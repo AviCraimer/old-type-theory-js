92cb0bfcccf3199b7108815f43d9579a3bb2f30b
AVI CRAIMER <AVI.CRAIMER@cbc.ca>
2020-12-01 23:07:37 -0500

    Added factory functions for variables, terms, and types

--- src/libraries/simpleTypeTheory/new/factories.js
+++ src/libraries/simpleTypeTheory/new/factories.js
@@ -1,3 +1,63 @@
-export default {}
+import symbols from "./symbols";
+const {expression: expressionSym} = symbols;
+
+let  variableCounter = 0;
+export const makeVariable = () => {
+    const variableLetters = ["x","y","z"];
+    let name;
+
+    if (variableCounter < variableLetters.length) {
+        name = variableLetters[variableCounter];
+    } else {
+        name = "var_" + (variableCounter + 1);
+    }
+    //Increment the variable counter
+    variableCounter++;
+
+    return {
+        name: name,
+        [expressionSym.key]: expressionSym.term.variable,
+        toString: () => name
+    }
+}
+
+let  termCounter = 0;
+export const makeConcreteTerm = ( ) => {
+    const termLetters = ["a","b","c", "d", "e"];
+    let name;
+
+    if (termCounter < termLetters.length) {
+        name = termLetters[termCounter];
+    } else {
+        name = "term_" + (termCounter + 1);
+    }
+    //Increment the term counter
+    termCounter++;
+
+    return {
+        name: name,
+        [expressionSym.key]: expressionSym.term.concrete,
+        toString: () => name
+    }
+}
+
+
+let  typeCounter = 0;
+export const makePrimitiveType = ( ) => {
+    const name = "Type_" + (typeCounter + 1);
+
+    //Increment the type counter
+    typeCounter++;
+
+    return {
+        name: name,
+        [expressionSym.key]: expressionSym.type.primitive,
+        toString: () => name
+    }
+}
+
+
+
+
 
 
--- src/libraries/simpleTypeTheory/tests/factories.test.js
+++ src/libraries/simpleTypeTheory/tests/factories.test.js
@@ -1,9 +1,47 @@
-import factories  from "../new/factories";
+import {makeVariable, makeConcreteTerm, makePrimitiveType}  from "../new/factories";
+import symbols from "../new/symbols";
 
 describe("Factory function tests", () => {
-    it("first", () => {
-        expect(1).toBe(1)
-    }  )
+    it("makeVariable names", () => {
+        let vars = [1,1,1,1,1].map(x => makeVariable() )
+        const [x,y,z,var_4, var_5] = vars;
+        const obj =  {x,y,z,var_4, var_5};
+        Object.entries(obj).forEach( ([varName, v]) =>  {
+            expect(varName).toBe(v.name);
+        } );
+    })
+    it("makeVariable has correct symbols", () => {
+        const x = makeVariable();
 
-})
+        expect(symbols.expression.key in x).toBe(true);
+        expect(x[symbols.expression.key]).toBe(symbols.expression.term.variable);
+    });
+    it("makeConcreteTerm names", () => {
+        let terms = [1,1,1,1,1,1].map(x => makeConcreteTerm() )
+        const [a, b, c, d, e, term_6] = terms;
+        const obj =  {a, b, c, d, e, term_6};
+        Object.entries(obj).forEach( ([termName, term]) =>  {
+            expect(termName).toBe(term.name);
+        } );
+    })
+    it("makeConcreteTerm has correct symbols", () => {
+        const t = makeConcreteTerm();
+
+        expect(symbols.expression.key in t).toBe(true);
+        expect(t[symbols.expression.key]).toBe(symbols.expression.term.concrete);
+    });
+    it("makePrimitiveType name", () => {
+        const T1 = makePrimitiveType();
+        const T2 = makePrimitiveType();
+
+        expect(T1.name).toBe("Type_1");
+        expect(T2.name).toBe("Type_2");
+    });
+    it("makePrimitiveType symbols", () => {
+        const t = makePrimitiveType();
+
+        expect(symbols.expression.key in t).toBe(true);
+        expect(t[symbols.expression.key]).toBe(symbols.expression.type.primitive);
+    });
+});
 
